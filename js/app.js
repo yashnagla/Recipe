f3c0462224eec8c70b242a838ce5912dfade3969
Yash Sikhwal Nagla <84613654+yashnagla@users.noreply.github.com>
2024-10-14 17:44:23 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const getElement = (selector) =>{
+  const element = document.querySelector(selector);
+  if(element) return element
+  throw Error(`Please Double check your class names, there is no  ${selector} class.`);
+}
+
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+
+navBtnDOM.addEventListener('click',()=>{
+  links.classList.toggle('show-links')
+})
+
+// Date
+const date = getElement('#date');
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
